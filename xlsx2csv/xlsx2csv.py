a160a389d5b6aa5308aad5b8add44523800974f5
cyjia <chaoyang_jia@163.com>
2014-07-21 22:56:48 +0800

    Add comment for function xlsx2csv

--- xlsx2csv/xlsx2csv.py
+++ xlsx2csv/xlsx2csv.py
@@ -1,3 +1,11 @@
-def xlsx2csv(file)
+def xlsx2csv(file, colnames=[])
+'''Given a excel file with tabula data in first sheet, copy the data into another csv file. 
+   
+   The name for csv file should be the same as original excel file.
+
+   The column names will be changed as provided names, if no colnames provide original column names will be used.
+
+   TODO: How to convert cell value to string?
+'''
 
 
